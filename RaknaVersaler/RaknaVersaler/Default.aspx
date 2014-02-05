2493450a94e4156f3dd2761e3de5c11d167afcda
tobbew <torbjorn@wastgard.se>
2014-02-05 10:24:03 +0100

    Upload1

---
+++ RaknaVersaler/RaknaVersaler/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RaknaVersaler.Default" ViewStateMode="Disabled" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hur många versaler?</title>
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <p class="rubrik">Räkna versaler</p>
+        <asp:TextBox ID="MainTextBox" runat="server" TextMode="MultiLine"></asp:TextBox>
+                
+        <asp:Button ID="CountButton" runat="server" Text="Räkna versaler" OnClick="CountButton_Click" CssClass="knapp" />
+        
+        <asp:Label ID="Resultat" runat="server" Text="" CssClass="resultat"></asp:Label>
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ RaknaVersaler/RaknaVersaler/Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using RaknaVersaler.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RaknaVersaler
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CountButton_Click(object sender, EventArgs e)
+        {
+            
+            if (MainTextBox.Enabled == false)
+            {
+
+                MainTextBox.Text = "";
+                MainTextBox.Enabled = true;
+                CountButton.Text = "Räkna versaler";
+                
+            }
+            
+            else
+            { 
+                var NumCapitals = new TextAnalyzer
+                {
+                    StringToCount = MainTextBox.Text
+
+                };
+
+                Resultat.Text = NumCapitals.GetNumberOfCapitals().ToString();
+                MainTextBox.Enabled = false;
+                CountButton.Text = "Clear";
+                
+
+            }
+        }
+    }
+}
